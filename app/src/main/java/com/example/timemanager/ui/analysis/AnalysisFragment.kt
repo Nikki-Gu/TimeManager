3371c120c28b2ddf05857259bdf56be3ec8ca773
105gun <ooo693428@163.com>
2021-11-14 16:14:09 +0800

    重构了一下页面，加了日历的模板

--- app/src/main/java/com/example/timemanager/ui/analysis/AnalysisFragment.kt
+++ app/src/main/java/com/example/timemanager/ui/analysis/AnalysisFragment.kt
@@ -10,6 +10,12 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.timemanager.R
 import com.example.timemanager.databinding.FragmentAnalysisBinding
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
+import java.time.YearMonth
+import java.time.temporal.WeekFields
+import java.util.*
 
 class AnalysisFragment : Fragment() {
 
@@ -32,13 +38,35 @@ class AnalysisFragment : Fragment() {
         val root: View = binding.root
         val toolbar = binding.homeToolbar
 
-        val textView: TextView = binding.textAnalysis
-        analysisViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
 
         // toolbar设置
         toolbar.title = "数据统计"
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            val textView = view.findViewById<TextView>(R.id.calendarDayText)
+
+            // With ViewBinding
+            // val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
+        }
+        val calendarView=binding.calendarView
+        calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+            // Called only when a new container is needed.
+            override fun create(view: View) = DayViewContainer(view)
+
+            // Called every time we need to reuse a container.
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.textView.text = day.date.dayOfMonth.toString()
+            }
+        }
+        val currentMonth = YearMonth.now()
+        val firstMonth = currentMonth.minusMonths(10)
+        val lastMonth = currentMonth.plusMonths(10)
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
+        calendarView.scrollToMonth(currentMonth)
+
+
+
         return root
     }
 
