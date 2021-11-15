0195197b779ab356ffaa7f0c1fd75f7c64b61d5f
chandlerping <18307130025@fudan.edu.cn>
2021-11-15 16:37:18 +0800

    数据库用户mapper

--- app/src/main/java/com/example/timemanager/repository/mapper/UserMapper.kt
+++ app/src/main/java/com/example/timemanager/repository/mapper/UserMapper.kt
@@ -1,4 +1,26 @@
 package com.example.timemanager.repository.mapper
 
+import com.example.timemanager.db.entity.UserEntity
+import com.example.timemanager.db.model.User
+
 object UserMapper {
+    fun UserEntity?.toDomain(): User? = this?.let{
+        User(
+            id = it.id,
+            name = it.name,
+            date = it.date,
+            timesFocus = it.timesFocus,
+            totalDuration = it.totalDuration
+        )
+    }
+
+    fun User?.toDomain(): UserEntity? = this?.let{
+        UserEntity(
+            id = it.id,
+            name = it.name,
+            date = it.date,
+            timesFocus = it.timesFocus,
+            totalDuration = it.totalDuration
+        )
+    }
 }
